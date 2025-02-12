dd81c498740f1666b9bc91d357a3b6e45878cc8f
Oghenerukevwe Esemitodje <o.esemitodje@gmail.com>
2025-02-12 15:49:27 +0100

    Create getHumanChoice function

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ["rock", "paper", "scissors"];
 
   return choices[randomNumber];
+}
+
+function getHumanChoice() {
+  const humanChoice = prompt(
+    "Choose Rock, Paper, or Scissors to play"
+  ).toLowerCase();
+
+  if (
+    humanChoice !== "rock" &&
+    humanChoice !== "paper" &&
+    humanChoice !== "scissors"
+  ) {
+    alert("Invalid choice! Please enter Rock, Paper, or Scissors.");
+    return getHumanChoice();
+  } else {
+    return humanChoice;
+  }
 }
