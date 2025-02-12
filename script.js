54616188c57ad8d5087a9ca45f20642a494884f9
Oghenerukevwe Esemitodje <o.esemitodje@gmail.com>
2025-02-12 16:02:33 +0100

    Create playRound function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let computerScore = 0;
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
 
   return choices[randomNumber];
 }
@@ -21,6 +21,24 @@ function getHumanChoice() {
     alert("Invalid choice! Please enter Rock, Paper, or Scissors.");
     return getHumanChoice();
   } else {
-    return humanChoice;
+    return humanChoice[0].toUpperCase() + humanChoice.subsrting(1);
   }
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie!, You both chose ${humanChoice}.`);
+  } else if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    console.log(`You win!, ${humanChoice} beats ${computerChoice}.`);
+    humanScore++;
+  } else {
+    console.log(`You lose!, ${computerChoice} beats ${humanChoice}.`);
+    computerScore++;
+  }
+
+  console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
 }
